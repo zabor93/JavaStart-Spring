f7d932fa7924327fbaf458353436a54d5500ecd2
Jakub Zaboronek <jakub.zaboronek@wp.pl>
2019-10-17 22:14:17 +0200

    test commit

--- springFrameworkEx1.1/src/main/java/Main.java
+++ springFrameworkEx1.1/src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         MessagePrinter printer = new MessagePrinter();
         printer.printMessage();
+        System.out.println("test");
     }
 }
